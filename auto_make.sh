f565da1d5b2f548eaf6a801921aaac4d6163dc69
Taoge <me@heytaoge.com>
2019-12-03 09:16:28 +0800

    - remove build

--- auto_make.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-fswatch -d taoge_blog |(while read;date; echo "*** Realoading ***"; do make html ;done)
--- upload.sh
+++
@@ -1,5 +0,0 @@
-rm -rf dist
-rm -rf taoge_blog.egg-info
-source venv/bin/activate
-python setup.py sdist
-twine upload dist/*
